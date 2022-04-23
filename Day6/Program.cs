a146e17be6955ae7d12ef6fb145b23d4ffef08c6
suraj <soorajkrpandit@gmail.com>
2022-04-23 11:11:39 +0530

    Temperature Conversion is added

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -23,14 +23,21 @@ namespace Day6
             //obj5.stopwatchmethod();
 
 
-            Console.WriteLine("1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes can be returned by Vending Machine.");
-            Console.WriteLine("Enter the Change amount that you want ");
-            int V = Convert.ToInt32(Console.ReadLine());
-            int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-            int m = Notes.Length;
-            VendorMachine obj6 = new VendorMachine();
-            Console.WriteLine("Minimum Notes required is " + obj6.minNotes(Notes, m, V));
-            
+            //Console.WriteLine("1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes can be returned by Vending Machine.");
+            //Console.WriteLine("Enter the Change amount that you want ");
+            //int V = Convert.ToInt32(Console.ReadLine());
+            //int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            //int m = Notes.Length;
+            //VendorMachine obj6 = new VendorMachine();
+            //Console.WriteLine("Minimum Notes required is " + obj6.minNotes(Notes, m, V));
+
+
+            Console.WriteLine("Enter 0 for CELCIUS to FAHRENHEIT CONVERSION & Enter 1 for FAHRENHEIT to CELCIUS CONVERSION ");
+            int Num4 = Convert.ToInt32(Console.ReadLine());
+            TempClass obj8 = new TempClass();
+            obj8.TempConv(Num4);
+           
+
 
             Console.ReadLine();
 
---
+++ Day6/TempClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class TempClass
+    {
+        public void TempConv(int Num4)
+        {
+
+            double c, f;
+
+            switch (Num4)
+            {
+                case 0:
+                    Console.WriteLine("Enter any Temperature value in celcius");
+
+                    c = Convert.ToDouble(Console.ReadLine());
+
+                    f = c * 9 / 5 + 32;
+
+                    Console.WriteLine("Temperature in FAHRENHEIT " + f);
+
+                    break;
+
+                case 1:
+                    Console.WriteLine("Enter any Temperature value in Fahrenheit");
+
+                    f = Convert.ToDouble(Console.ReadLine());
+
+                    c = (f - 32) * 5 / 9;
+
+                    Console.WriteLine("Temperature in CELCIUS " + c);
+
+                    break;
+
+            }
+        }
+    }
+}
