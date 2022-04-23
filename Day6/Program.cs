eabd0b46a626e650de1ad14286a9cb88c40b4ebf
suraj <soorajkrpandit@gmail.com>
2022-04-23 11:05:21 +0530

    Vendor Machine is added

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -19,8 +19,18 @@ namespace Day6
             //ReverseNum.reversenumbermethod();
 
 
-            watchclass obj5 = new watchclass();
-            obj5.stopwatchmethod();
+            //watchclass obj5 = new watchclass();
+            //obj5.stopwatchmethod();
+
+
+            Console.WriteLine("1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes can be returned by Vending Machine.");
+            Console.WriteLine("Enter the Change amount that you want ");
+            int V = Convert.ToInt32(Console.ReadLine());
+            int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int m = Notes.Length;
+            VendorMachine obj6 = new VendorMachine();
+            Console.WriteLine("Minimum Notes required is " + obj6.minNotes(Notes, m, V));
+            
 
             Console.ReadLine();
 
