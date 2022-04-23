64774b5e5a9b0a896da930398ad3cab07fa8a97f
suraj <soorajkrpandit@gmail.com>
2022-04-23 11:42:58 +0530

    Square Root is added

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -10,48 +10,72 @@ namespace Day6
     {
         static void Main(string[] args)
         {
-            //Fibonum.fibonumber();
+            //  Console.WriteLine("for fibo number.................................");
 
-            //PerfectNum.perfectnumbermethod();
-
-            //primeNum.primenumbermethod();
-
-            //ReverseNum.reversenumbermethod();
+            /*  Fibonum.fibonumber();   */
 
 
-            //watchclass obj5 = new watchclass();
-            //obj5.stopwatchmethod();
+            //   Console.WriteLine("for perfect number.................................");
+
+            /*   PerfectNum.perfectnumbermethod();    */
 
 
-            //Console.WriteLine("1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes can be returned by Vending Machine.");
-            //Console.WriteLine("Enter the Change amount that you want ");
-            //int V = Convert.ToInt32(Console.ReadLine());
-            //int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-            //int m = Notes.Length;
-            //VendorMachine obj6 = new VendorMachine();
-            //Console.WriteLine("Minimum Notes required is " + obj6.minNotes(Notes, m, V));
+            //   Console.WriteLine("for prime number.....................................");
+
+            /*  primeNum.primenumbermethod();      */
 
 
-            //Console.WriteLine("Enter 0 for CELCIUS to FAHRENHEIT CONVERSION & Enter 1 for FAHRENHEIT to CELCIUS CONVERSION ");
-            //int Num4 = Convert.ToInt32(Console.ReadLine());
-            //TempClass obj8 = new TempClass();
-            //obj8.TempConv(Num4);
+            //   Console.WriteLine("for reverse number....................................");
 
-            Console.WriteLine("Enter the Day Number");
+            /*   ReverseNum.reversenumbermethod();     */
 
-            int D = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the month in number");
+            //   Console.WriteLine("for stop watch.......................................");
 
-            int M = Convert.ToInt32(Console.ReadLine());
+            /*   watchclass obj5 = new watchclass();
+                 obj5.stopwatchmethod();     */
 
-            Console.WriteLine("Enter the Year");
 
-            int Y = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("for vending machine......................................");
 
-            DayOfWeek obj7 = new DayOfWeek();
+            /*    Console.WriteLine("1, 2, 5, 10, 50, 100, 500 and 1000 Rs Notes can be returned by Vending Machine.");
+                  Console.WriteLine("Enter the Change amount that you want ");
+                  int V = Convert.ToInt32(Console.ReadLine());
+                  int[] Notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+                  int m = Notes.Length;
+                  VendorMachine obj6 = new VendorMachine();
+                  Console.WriteLine("Minimum Notes required is " + obj6.minNotes(Notes, m, V));    */
 
-            obj7.dayOfWeek(D, M, Y);
+
+            //    Console.WriteLine("for temp conversion...................................");
+
+            /*     Console.WriteLine("Enter 0 for CELCIUS to FAHRENHEIT CONVERSION & Enter 1 for FAHRENHEIT to CELCIUS CONVERSION ");
+                   int Num4 = Convert.ToInt32(Console.ReadLine());
+                   TempClass obj8 = new TempClass();
+                   obj8.TempConv(Num4);      */
+
+
+            //    Console.WriteLine("for Day of week...............................................");
+
+            /*    Console.WriteLine("Enter the Day Number");
+                  int D = Convert.ToInt32(Console.ReadLine());
+                  Console.WriteLine("Enter the month in number");
+                  int M = Convert.ToInt32(Console.ReadLine());
+                  Console.WriteLine("Enter the Year");
+                  int Y = Convert.ToInt32(Console.ReadLine());
+                  DayOfWeek obj7 = new DayOfWeek();
+                  obj7.dayOfWeek(D, M, Y); */
+
+            //   Console.WriteLine("for sqaure of root............................");
+
+            /*   int Count3 = 0;
+                Console.WriteLine(" \n Welcome to Sqrt Program using Newtons Method ...");
+                Console.WriteLine("Enter any number to get it's SQUAREROOT ");
+                double Num5 = Convert.ToInt32(Console.ReadLine()); 
+                SqaureRoot obj9 = new SqaureRoot();
+                obj9.Sqrt(Count3, Num5);          */
+
+
 
             Console.ReadLine();
 
